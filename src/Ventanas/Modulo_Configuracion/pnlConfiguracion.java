4bf067b7bb68dc707adef2af43b8ff630e4839da
Jonathan <jvaldez_2610@hotmail.com>
2020-07-09 21:33:56 -0500

    commit

--- src/Ventanas/Modulo_Configuracion/pnlConfiguracion.java
+++ src/Ventanas/Modulo_Configuracion/pnlConfiguracion.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Ventanas.Modulo_Configuracion;
 
 import A_tabla.*;
-import Clases.Cita;
 import Clases.Conexion;
 import Ventanas.Modulo_Usuario.pnlPass;
 import Ventanas.Modulo_Usuario.pnlUser;
