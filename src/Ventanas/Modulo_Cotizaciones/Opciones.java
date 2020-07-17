d4e936c1a1dc1ec90783e5ecf589a73f54b9afdf
RVZ20 <rvc28896@gmail.com>
2020-07-16 21:55:11 -0500

    servicios

--- src/Ventanas/Modulo_Cotizaciones/Opciones.java
+++ src/Ventanas/Modulo_Cotizaciones/Opciones.java
@@ -1,6 +1,7 @@
 package Ventanas.Modulo_Cotizaciones;
 
 import Clases.Conexion;
+import static Ventanas.Modulo_Ruta_Cotizacion.Opciones.cn;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -189,6 +190,51 @@ public class Opciones {
                             System.out.println(e);
                         }
 
+    }
+    public static void insertarServicio(int ID_Cotizacion){
+        
+           String q = " INSERT INTO servicios (ID_Servicio,ID_Cotizacion,Nombre_Servicio,Precio)"
+                                        + "VALUES (NULL,'"+ID_Cotizacion+"','',0)";      
+                                        try {PreparedStatement pstm = cn.prepareStatement(q);
+                                    pstm.execute();
+                                    pstm.close();
+                                    }catch(SQLException e){            
+                                    System.out.println(e);}
+                                        JOptionPane.showMessageDialog(null, "Desde insertar servicio id:"+ID_Cotizacion);
+    }
+    
+    public static void llenarServicio(int ID_Cotizacion){
+
+
+    DefaultTableModel modelo = (DefaultTableModel) Ventanas.Modulo_Cotizaciones.AgregarCotizaciones.tabla1.getModel();
+
+        while (modelo.getRowCount() > 0) {
+            modelo.removeRow(0);
+        }
+        
+        String sql = "";
+        
+            sql = "Select ID_Servicio, ID_Cotizacion, Nombre_Servicio,Precio from servicios where ID_Cotizacion =" + ID_Cotizacion;
+        
+        String datos[] = new String[4];
+        try {           
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) 
+            {
+                datos [0] = String.valueOf(rs.getInt(1));
+                datos [1] = rs.getString(2);
+                datos [2] = rs.getString(3);
+                datos [3] = rs.getString(4);
+                
+                modelo.addRow(datos);
+            }
+            
+            modelo.fireTableDataChanged();
+        } catch (SQLException ex) {
+            Logger.getLogger(Opciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        JOptionPane.showMessageDialog(null, "Desde llenar servicio id:"+ID_Cotizacion);
     }
    
     
