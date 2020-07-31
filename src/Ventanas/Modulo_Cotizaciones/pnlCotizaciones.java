690cf67e1fa3518bff91cb817fd84984958ec914
Maricruz Valdez <isc_valdez.m@tesvb.edu.mx>
2020-07-31 00:09:46 -0500

    2

--- src/Ventanas/Modulo_Cotizaciones/pnlCotizaciones.java
+++ src/Ventanas/Modulo_Cotizaciones/pnlCotizaciones.java
@@ -292,7 +292,9 @@ public class pnlCotizaciones extends javax.swing.JPanel {
         });
         jScrollPane1.setViewportView(tabla);
         if (tabla.getColumnModel().getColumnCount() > 0) {
+            tabla.getColumnModel().getColumn(0).setMinWidth(0);
             tabla.getColumnModel().getColumn(0).setPreferredWidth(0);
+            tabla.getColumnModel().getColumn(0).setMaxWidth(0);
             tabla.getColumnModel().getColumn(1).setResizable(false);
             tabla.getColumnModel().getColumn(1).setPreferredWidth(230);
             tabla.getColumnModel().getColumn(2).setResizable(false);
