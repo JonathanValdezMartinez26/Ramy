41042fea4038c892e24b5262c2149e20b7f9e952
Jonathan <jvaldez_2610@hotmail.com>
2020-09-30 15:35:05 -0500

    fin

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -21,7 +21,7 @@ public void Conectar(String user,String pass)throws SQLException,ClassNotFoundEx
             
             
     //con = DriverManager.getConnection("jdbc:mysql://localhost/clinica?schema&sessionVariables=wait_timeout=600&confluence&autoReconnect=true&useUnicode=true&useJDBCCompilantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useInformationSchema=true",user, pass);            
-    con = DriverManager.getConnection("jdbc:mysql://localhost/clinica?useUnicode=true&characterEncoding=utf-8",user, pass);        
+    con = DriverManager.getConnection("jdbc:mysql://192.168.0.14/clinica?useUnicode=true&characterEncoding=utf-8",user, pass);        
     state=con.createStatement(result.TYPE_SCROLL_SENSITIVE,result.CONCUR_UPDATABLE);
 
 //    con = DriverManager.getConnection("jdbc:mysql://192.168.0.14/clinica?schema&sessionVariables=wait_timeout=600&confluence&autoReconnect=true&useUnicode=true&useJDBCCompilantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useInformationSchema=true",user, pass);            
@@ -49,7 +49,7 @@ public void Conectar(String user,String pass)throws SQLException,ClassNotFoundEx
         
         
         //con = DriverManager.getConnection("jdbc:mysql://localhost/clinica?schema&sessionVariables=wait_timeout=600&confluence&autoReconnect=true&useUnicode=true&useJDBCCompilantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useInformationSchema=true","root", "");            
-        con = DriverManager.getConnection("jdbc:mysql://localhost/clinica?useUnicode=true&characterEncoding=utf-8","root", "");
+        con = DriverManager.getConnection("jdbc:mysql://192.168.0.14/clinica?useUnicode=true&characterEncoding=utf-8","root", "");
 
 //        con = DriverManager.getConnection("jdbc:mysql://192.168.0.14/clinica?schema&sessionVariables=wait_timeout=600&confluence&autoReconnect=true&useUnicode=true&useJDBCCompilantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useInformationSchema=true","root", "");            
 
